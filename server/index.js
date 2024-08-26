9342207b38c41e08975f9d358b2880207ecec874
Darshan Nayak <nayakdarshan22@gmail.com>
2024-08-27 05:05:36 +0530

    BEC 68

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
   app.get('/', (req, res) => {
     res.send('Welcome to the backend!');
-    console.log('BACKEND SERVER RUNNING');
+    // console.log('BACKEND SERVER RUNNING');
   });
 // Define Routes
 app.use('/api/v1', require('./routes/portfolioRoute'));

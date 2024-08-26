10a7c2bce65032e6028f21cc7d7be039d821d42a
Darshan Nayak <nayakdarshan22@gmail.com>
2024-08-27 02:23:23 +0530

    CI/CD check 2

--- client/src/pages/Home/Intro.js
+++ client/src/pages/Home/Intro.js
@@ -7,7 +7,7 @@ function Intro() {
         <div className='d-flex align-items-center justify-content-center'>
             <div className='d-flex align-items-center justify-content-center flex-column'>
                 {/* <div className='fs-24 fw-500 text-center'>Hello! I am</div> */}
-                <div className='fs-80 fw-500 text-center font-new lh-80'>Full Stack Developer</div>
+                <div className='fs-80 fw-500 text-center font-new lh-80'>Frontend Developer</div>
                 <div className='fs-24 fw-500 text-center w-75 mb-2'>I design and code beautifully simple things, and I love what I do.</div>
                 <div className='profile-img-container mt-5'>
                     <img src={profileImg} loading='lazy' alt='darshan' className='profile-img'/>

fba1091c84005f51c053f7f08b97847241415ea2
Darshan Nayak <nayakdarshan22@gmail.com>
2024-09-19 18:22:52 +0530

    CORS fix 7

--- server/index.js
+++ server/index.js
@@ -7,14 +7,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://www.darshannayak.in','http://staging.darshannayak.in'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
-app.options('*', cors());
-
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })

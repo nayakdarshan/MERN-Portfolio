bac8c55061a7e98a32a5d3f45ecc56cd8eaf40e5
Darshan Nayak <nayakdarshan22@gmail.com>
2024-09-19 18:28:45 +0530

    CORS fix 8

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://staging.darshannayak.in', 'https://darshannayak.in'],
+}));
 app.use(express.json());
 
 // Connect to MongoDB

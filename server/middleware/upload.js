99601816b1c0e4901e8fcb47d4ac106cbee6b9f8
Darshan Nayak <nayakdarshan22@gmail.com>
2024-09-21 16:46:55 +0530

    dep functionality fix 4

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,15 +1,15 @@
-const AWS = require('aws-sdk');
-const multer = require('multer');
+const { S3Client } = require('@aws-sdk/client-s3');
+const { Upload } = require('@aws-sdk/lib-storage');
 const multerS3 = require('multer-s3');
-const path = require('path');
 
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,          
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, 
-  region: process.env.AWS_REGION,                      
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
-const s3 = new AWS.S3();
 
 // Configure multer-s3 storage
 const upload = multer({
